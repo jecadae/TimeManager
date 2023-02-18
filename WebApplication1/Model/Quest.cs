c69510520f4a43b4f79b9dd3c0d05115721c141e
jecadae <80022211+jecadae@users.noreply.github.com>
2023-02-18 17:13:43 +0700

    Update Quest.cs

--- WebApplication1/Model/Quest.cs
+++ WebApplication1/Model/Quest.cs
@@ -2,5 +2,10 @@
 
 public class Quest
 {
-    
+    public Guid Id { get;set; }
+    public string name{get;set;}
+    public string surname{get;set;}
+    public string sur{get;set;}
+    public string password{get;set;}
+    public string email{get;set;}
 }
