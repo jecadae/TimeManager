9cb6f9bb2bc858db235bfbfb415a9fc2b52b2796
BolshakovDmt <bolshakovdmt@gmail.com>
2023-02-18 17:25:20 +0700

    Front
    хуету дрпопнул

---
+++ WebApplication1/Resources/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo:'/login', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
