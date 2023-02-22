597bd8ee43a6550cba8926272060121f15d4d70f
BolshakovDmt <bolshakovdmt@gmail.com>
2023-02-22 13:30:53 +0700

    Registration route

--- WebApplication1/Resources/src/app/app-routing.module.ts
+++ WebApplication1/Resources/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { PasswordResetComponent } from './components/password-reset/password-reset.component';
+import { Registration } from './components/registration/registration';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: '', redirectTo:'/login', pathMatch: 'full'},
   {path: '***', component: NotFoundComponent},
-  {path: 'password-reset', component: PasswordResetComponent}
+  {path: 'password-reset', component: PasswordResetComponent},
+  {path: 'registration', component: Registration}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
