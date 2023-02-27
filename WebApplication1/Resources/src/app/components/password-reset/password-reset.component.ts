a7330565692010fad091d42f3bb2b3031690c388
BolshakovDmt <bolshakovdmt@gmail.com>
2023-02-27 14:35:22 +0700

    email check

--- WebApplication1/Resources/src/app/components/password-reset/password-reset.component.ts
+++ WebApplication1/Resources/src/app/components/password-reset/password-reset.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./password-reset.component.css']
 })
 export class PasswordResetComponent {
+  constructor() { }
+
+  checkNullEmail(email: string) {
+
+    if (email == '') {
+      return false
+    }
+    else return true;
+
+  }
+
+  checkEmail(email: string) {
+    var lowerRussianLetters = /[а-я]/g
+    var upperRussianLetters = /[А-Я]/g
+
+    if (email.indexOf('@') != -1 
+    && (email.indexOf('.com') != -1 || email.indexOf('.ru') != -1)
+    && email.match(lowerRussianLetters) == null
+    && email.match(upperRussianLetters) == null
+    ) return true
+    else return false
+  }
 
 }
+
+
