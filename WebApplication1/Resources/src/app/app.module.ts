d721bf3d8f0cacbaaeba3f0d49351c5e1291bae0
BolshakovDmt <bolshakovdmt@gmail.com>
2023-03-02 14:34:16 +0700

    search bar

--- WebApplication1/Resources/src/app/app.module.ts
+++ WebApplication1/Resources/src/app/app.module.ts
@@ -20,6 +20,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { HomePageComponent } from './home-page/home-page.component'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 @NgModule({
@@ -45,7 +48,9 @@ import { HomePageComponent } from './home-page/home-page.component';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatFormFieldModule, 
+    MatInputModule
   ],
 
   providers: [    
--- WebApplication1/Resources/src/app/navigation-bar/navigation-bar.component.ts
+++ WebApplication1/Resources/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,8 +1,17 @@
+import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+const material = [
+  MatFormFieldModule, 
+  MatInputModule
+]
+
 
 @Component({
   selector: 'app-navigation-bar',
