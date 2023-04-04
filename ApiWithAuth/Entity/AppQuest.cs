1727bbf65efd10b133c4ffabd2882afe5ac78404
jecadae <80022211+jecadae@users.noreply.github.com>
2023-04-04 17:09:21 +0700

    12

--- ApiWithAuth/Entity/AppQuest.cs
+++ ApiWithAuth/Entity/AppQuest.cs
@@ -3,10 +3,10 @@
 public class AppQuest   
 {
     public int Id { get; set; } 
-    public String Parent  { get; set; }
-
+    public int Parent  { get; set; }
+    public string Discription{ get; set; }
     public DateTime DeadLine { get; set; }
-
+    private bool priv { get; set; } = false;
     public bool Status { get; set; } = false;
     public ICollection<Link> Links { get; set; } = new List<Link>();
 }
--- ApiWithAuth/Entity/Link.cs
+++ ApiWithAuth/Entity/Link.cs
@@ -2,8 +2,7 @@ namespace ApiWithAuth.Entity;
 
 public class Link
 {
-    public bool priv { get; set; } = false;
+
     public string url{ get; set; } 
     
-
 }
