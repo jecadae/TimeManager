e4d24b20ce576562cf0add3585af1e73d4f25f8e
bandimaf <bandimaf@gmail.com>
2023-04-30 16:40:20 +0700

    1

--- ApiWithAuth/Resources/src/app/components/modal-window/modal-window.component.ts
+++ ApiWithAuth/Resources/src/app/components/modal-window/modal-window.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { Router } from '@angular/router'
+import { HttpClient } from '@angular/common/http';
+import { ViewChild, AfterViewInit } from '@angular/core';
 
 const material = [
   MatFormFieldModule,
@@ -15,16 +18,25 @@ const material = [
 })
 
 
-export class ModalWindowComponent {
+export class ModalWindowComponent implements OnInit, AfterViewInit {
+
+  // @ViewChild('test') test: any;
 
   constructor(public dialogRef: MatDialogRef<ModalWindowComponent>) { }
   value: any = 'Clear me';
 
+  ngAfterViewInit() {
+
+    // if (this.checkDeadline = !this.checkDeadline) {
+    //   this.test.nativeElement
+    // }
+  }
   ngOnInit() { }
 
   privacy: string = '';
   choicePrivacy: string[] = ['Скрыть задачу', 'Сделать задачу видимой'];
 
+  checkDeadline: boolean = false;
   nameGoal: any = undefined;
   taskName: any = undefined;
   deadline: any = undefined;
