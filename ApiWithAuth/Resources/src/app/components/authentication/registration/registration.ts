2aa0321719e102d8d257939dc0d2ae0e540d3b0d
BolshakovDmt <bolshakovdmt@gmail.com>
2023-05-02 12:25:25 +0700

    login link

--- ApiWithAuth/Resources/src/app/components/authentication/registration/registration.ts
+++ ApiWithAuth/Resources/src/app/components/authentication/registration/registration.ts
@@ -102,7 +102,7 @@ export class RegistrationComponent implements OnInit {
       }
       else {
         alert('Регистрация прошла успешно')
-        this.router.navigate(['/login']);
+        this.router.navigate(['localhost:4200/login']);
       }
     })
     return
